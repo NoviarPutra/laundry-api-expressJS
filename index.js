f6ab27fe620137062489a1e0b051de3ecf8f7484
NoviarPutra <noviarpb@gmail.com>
2022-10-06 16:39:21 +0700

    fix

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 const { default: mongoose } = require("mongoose");
 const router = require("./src/router");
 const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 const PORT = process.env.PORT;
 const URI = process.env.URI;
 
@@ -26,13 +22,6 @@ mongoose
     console.log(err);
   });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
